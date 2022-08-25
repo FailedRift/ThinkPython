a0346fe5ebc29de028a1eef4f254f9ab04b7bd0f
FailedRift <ikennaaokon@gmail.com>
2022-08-25 13:33:13 +0100

    Added function in_bisect()

--- ex10_10.py
+++ ex10_10.py
@@ -9,3 +9,21 @@ def make_word_list():
         word = i.strip()
         word_list.append(word)
     return word_list
+
+def in_bisect(word_list, word):
+    """Checks whether a word is in word_list using the bisect method.
+    
+    word_list: list of strings
+    word: string.
+    """
+    if len(word_list) == 0:
+        return False
+    
+    i = len(word_list)//2
+    if word_list[i] == word:
+        return True
+    
+    if word_list[i] > word:
+        return in_bisect(word_list[:i], word)
+    else:
+        return in_bisect(word_list[i:], word)
